9215dff9fc25312f024d5162b096b2d76c8cd360
syjoh <syjoh@DESKTOP-KJ0R4GI.lan1>
2020-04-12 17:59:41 -0700

    Now searches through URLs

--- WordScraper/src/ScrapeWebsite.java
+++ WordScraper/src/ScrapeWebsite.java
@@ -6,33 +6,68 @@ import java.nio.charset.StandardCharsets;
 import java.util.*;
 import static java.lang.System.*;
 
+import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 //import org.jsoup.select.Element;
 import org.jsoup.select.Elements;
 
 public class ScrapeWebsite {
+
+	/*
+	 public void allLinksInUrl() throws IOException {
+
+        Document doc = Jsoup.connect("https://www.wikipedia.org").get();
+
+        Elements links = doc.select("a[href]");
+
+        for (Element link : links) {
+
+            System.out.println("\nlink : " + link.attr("href"));
+
+            System.out.println("text : " + link.text());
+
+        }
+
+    }
+    */
+	
 	public static void main(String args[]){
 		print("running...");
 		Document document;
 		try {
 			//Get Document object after parsing the html from given url.
-	        document = Jsoup.connect("https://www.naver.com/").get();
+			String url = "https://m.blog.naver.com/PostView.nhn?blogId=boooovely&logNo=221890546101&searchKeyword=%EB%A9%94%EC%9D%B4%ED%81%AC%EC%97%85";
+	        document = Jsoup.connect(url).get();
+	        System.out.println(document.body().children().text());
+	        
+	        List<String> visitedUrls = new ArrayList<>();
+	        visitedUrls.add(url);
+	        Queue<Elements> q = new LinkedList<>();
+            Elements links = document.select("a[href]");
+            q.add(links);
+            while(!q.isEmpty())
+            {
+            	Elements curr_links = q.poll();
+    	        for (Element link : curr_links) {
+    	        	String next_url = link.absUrl("href");
+    	        	if(visitedUrls.contains(next_url))
+    	        	{
+    	        		continue;
+    	        	}
+    	        	else
+    	        	{
+    	        		visitedUrls.add(next_url);
+    	        		document = Jsoup.connect(next_url).get();
+    	        		System.out.println(document.body().children().text());
+    	        		Elements new_links = document.select("a[href]");
+    	        		q.add(new_links);
+    	        	}
+    	        }
+            }
 
-	        //Get the actual text from the page, excluding the HTML
-	        String text = document.body().text();
-	        System.out.println(text);
 			
-	        //testing, remove later
-			//Elements ps = doc.select("p");
-
-			//String title = document.title(); //Get title
-			//print("  Title: " + title); //Print title.
-
-			//Elements price = document.select("list-card-heading:contains($)"); //Get price
-			//Elements address = document.select("span[itemprop]:contains(DenverCO)"); //Get address
-			
-	        //output to csv eventually
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -42,5 +77,4 @@ public class ScrapeWebsite {
 	public static void print(String string) {
 		System.out.println(string);
 	}
-
 }
